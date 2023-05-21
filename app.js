0f3ef3e61ca6b5cc9badbe2550ac827c21960671
MuhammadAhmed120 <muhammadahmed120192@gmail.com>
2023-05-21 16:05:04 +0500

    Iron Man Complete

--- app.js
+++ app.js
@@ -8,47 +8,18 @@ var spiderOriginal = spiderChar.src;
 
 
 
-// else if (keyPressed === "w") {
-    //     setTimeout(function() {
-    //         ironChar.className = "iron"
-    //         ironChar.src = ironOriginal;
-    //         console.log(ironChar.src);
-    //     }, 3000);
-        
-    //     ironChar.className = "fly"
-    //     ironChar.src = "gifs/ironman-fly.gif";
-    //     console.log(ironChar.src);
-    // }
+
 
 
 
 
 function pressedKey(event) {
     var keyPressed = event.key;
-
-    if (keyPressed === "e") {
-        setTimeout(function() {
-            ironChar.className = "iron";
-            ironChar.src = ironOriginal;
-            console.log(ironChar.src);
-        }, 2500);
-
-        ironChar.className = "shield";
-        ironChar.src = "gifs/ironman-shields.gif";
-        console.log(ironChar.src);
-    }
-    if (keyPressed === "q") {
-        setTimeout(function() {
-            ironChar.src = ironOriginal;
-            console.log(ironChar.src);
-        }, 1500);
-
-        ironChar.src = "gifs/ironman-smart.gif";
-        console.log(ironChar.src);
-    }
-
     var parentWidth = ironDiv.parentElement.offsetWidth;
 
+
+
+    // MOVEMENT - IRONMAN
     if(keyPressed === "d") {
         var currentLeft = parseFloat(getComputedStyle(ironDiv).left); // left value in pixels.
         var percentLeft = Math.floor((currentLeft / parentWidth) * 100); // converting px into percentage
@@ -59,7 +30,6 @@ function pressedKey(event) {
         if(ironDiv.style.left === "81%"){
             ironDiv.style.left = "78%";
         }
-        console.log(percentLeft)
     }
 
     if(keyPressed === "a") {
@@ -70,9 +40,83 @@ function pressedKey(event) {
 
         if(ironDiv.style.left === "-3%"){
             ironDiv.style.left = "0%"
-        }
-
+        }   
     }
+
+    // COMBAT - IRONMAN
+    if (keyPressed === "q") {
+        setTimeout(function() {
+            ironChar.classList = "iron"
+            ironChar.src = ironOriginal;
+        }, 1500);
+
+        ironChar.className = "smart"
+        ironChar.src = "gifs/ironman-smart.gif";
+    }
+
+    if (keyPressed === "e") {
+        setTimeout(function() {
+            ironChar.className = "iron";
+            ironChar.src = ironOriginal;
+        }, 2500);
+
+        ironChar.className = "shield";
+        ironChar.src = "gifs/ironman-shields.gif";
+    }
+
+    if (keyPressed === "f") {
+        setTimeout(function() {
+            ironChar.className = "iron";
+            ironChar.src = ironOriginal;
+        }, 5000);
+
+        ironChar.className = "crouch";
+        ironChar.src = "gifs/ironman-crouch.gif";
+    }
+
+    if (keyPressed === "w") {
+        setTimeout(function() {
+            ironChar.className = "iron"
+            ironChar.src = ironOriginal;
+        }, 3000);
+        
+        ironChar.className = "fly"
+        ironChar.src = "gifs/ironman-fly.gif";
+    }
+
+
+
+
+
+
+
+    // MOVEMENT - SPIDERMAN
+    if(keyPressed === "ArrowLeft"){
+        var currentRight = parseFloat(getComputedStyle(spiderDiv).right);
+        var percentRight = Math.floor((currentRight / parentWidth) * 100);
+        percentRight += 3;
+        spiderDiv.style.right = `${percentRight}%`;
+
+        if(spiderDiv.style.right === "75%"){
+            spiderDiv.style.right = "72%";
+        }
+    }
+
+    if(keyPressed === "ArrowRight"){
+        var currentLeft = parseFloat(getComputedStyle(spiderDiv).right);
+        var percentLeft = Math.floor((currentLeft / parentWidth) * 100);
+        percentLeft -= 3;
+        spiderDiv.style.right = `${percentLeft}%`;
+
+        if(spiderDiv.style.right === "-3%"){
+            spiderDiv.style.right = "0%";
+        }
+    }
+
+
+
+    // COMBAT - SPIDERMAN
+
 }
 
 document.addEventListener("keyup", pressedKey);
